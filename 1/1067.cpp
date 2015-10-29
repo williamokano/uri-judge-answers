d60f515231b0493b4346cff6de9b6d4cb449050c
William <williamokano@gmail.com>
2015-10-29 14:37:59 -0200

    1067

---
+++ 1/1067.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+/* solucao 1067 */
+int main()
+{
+    int entrada;
+    cin >> entrada;
+
+    for (int i = 1; i <= entrada; i += 2) {
+        cout << i << endl;
+    }
+    return 0;
+}
