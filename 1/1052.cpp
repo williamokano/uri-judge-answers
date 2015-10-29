9870ecd0e2bac9f39c19ed0e86f4bf95f3106a8d
William <williamokano@gmail.com>
2015-10-29 11:48:25 -0200

    1052.cpp

---
+++ 1/1052.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+/* solucao 1052 */
+int main()
+{
+    int mes;
+    string meses[12] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+
+    cin >> mes;
+    mes--;
+
+    cout << meses[mes] << endl;
+
+    return 0;
+}
